7733bd646893e9bb555e12fffcfdac275f6c8958
HStakhiv <gsstahiv@gmail.com>
2022-11-14 12:10:27 +0200

    Add crispy forms and STATIC_ROOT to settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'todo',
 ]
 
@@ -120,6 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

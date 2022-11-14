64dbbec4cbda1a1b73855413b952d8a5b9c0e1ff
HStakhiv <gsstahiv@gmail.com>
2022-11-14 11:00:40 +0200

    Create Models and add app `todo` to the settings.py

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField()
+    deadline = models.DateTimeField()
+    status = models.BooleanField()
+    tags = models.ManyToManyField(to=Tag, related_name="task")

e98ad51aec3ba9b5b036cb1351e180690d7fd441
HStakhiv <gsstahiv@gmail.com>
2022-11-14 12:21:57 +0200

    Add TagUpdateView

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from .views import index, TagListView, TagCreateView
+from .views import (
+    index,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create/", TagCreateView.as_view(), name="tag-crate")
+    path("tags/create/", TagCreateView.as_view(), name="tag-crate"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update")
 ]
 
 app_name = "todo"

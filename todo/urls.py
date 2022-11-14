721de2d73c0cad02eacb89070abd5c2b14a20e66
HStakhiv <gsstahiv@gmail.com>
2022-11-14 12:11:30 +0200

    Add new tag_list and tag_creation path

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import index, TagListView, TagCreateView
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-crate")
 ]
 
 app_name = "todo"

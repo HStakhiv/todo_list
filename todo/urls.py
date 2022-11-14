c1d3badae3d3223a6fcd70b4b25e7c0839118f7a
HStakhiv <gsstahiv@gmail.com>
2022-11-14 13:02:17 +0200

    Add TaskCreateView

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (
     index,
+    TaskCreateView,
     TagListView,
     TagCreateView,
     TagUpdateView,
@@ -12,6 +13,7 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TaskCreateView.as_view(), name="task-crate"),
     path("tags/create/", TagCreateView.as_view(), name="tag-crate"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import TaskForm
 from .models import Tag, Task
 
 
@@ -13,6 +14,12 @@ def index(request):
     }
 
     return render(request, "todo/index.html", context)
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:index")
 
 
 class TagListView(generic.ListView):

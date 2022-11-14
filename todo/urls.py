c993f0cd54f456860335e1d2eb165e76cf9a0ac6
HStakhiv <gsstahiv@gmail.com>
2022-11-14 14:01:09 +0200

    Add toggle_for_tasks path

--- todo/urls.py
+++ todo/urls.py
@@ -7,16 +7,18 @@ from .views import (
     TagCreateView,
     TagUpdateView,
     TagDeleteView,
+    toggle_for_tasks,
 )
 
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("task/create/", TaskCreateView.as_view(), name="task-crate"),
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create/", TaskCreateView.as_view(), name="task-crate"),
     path("tags/create/", TagCreateView.as_view(), name="tag-crate"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/<int:pk>/toggle/", toggle_for_tasks, name="toggle")
 ]
 
 app_name = "todo"

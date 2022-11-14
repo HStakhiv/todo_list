9d008e98d944ffaad4bfce3054ba1d7fd18c5bed
HStakhiv <gsstahiv@gmail.com>
2022-11-14 13:55:11 +0200

    Update models.py: implement __str__ method and ordering

--- todo/models.py
+++ todo/models.py
@@ -4,10 +4,22 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
-    content = models.CharField(max_length=255)
-    datetime = models.DateTimeField()
-    deadline = models.DateTimeField()
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField()
     tags = models.ManyToManyField(to=Tag, related_name="task")
+
+    class Meta:
+        ordering = ["status", "-datetime"]
+
+    def __str__(self):
+        return f"{self.content} {self.status}"
